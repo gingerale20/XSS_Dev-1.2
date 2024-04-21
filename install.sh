5f524d59502abfcf7c5fce813e8dff8b4b9bd011
gingerale20 <159393754+gingerale20@users.noreply.github.com>
2024-04-20 23:12:01 -0400

    Create Install Scripts

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+cd /bin && sudo curl -O https://raw.githubusercontent.com/gingerale20/XSS_Dev-1.2/main/xss_dev
+cd
+cd /bin && sudo curl -O https://raw.githubusercontent.com/gingerale20/loaddelaylib3/main/delay.py
+cd
+sudo chmod 755 /bin/xss_dev
+echo '
+
+Installation Complete.
+'
