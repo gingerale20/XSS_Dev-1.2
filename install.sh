434dd1665623e4292265bf462fa8b69f06edf314
gingerale20 <159393754+gingerale20@users.noreply.github.com>
2024-04-21 10:42:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+# Library
+pip install urllib3
+# Main
 cd /bin && sudo curl -O https://raw.githubusercontent.com/gingerale20/XSS_Dev-1.2/main/xss_dev
 cd
+# Library
 cd /bin && sudo curl -O https://raw.githubusercontent.com/gingerale20/loaddelaylib3/main/delay.py
 cd
+# Disable access restrictions
 sudo chmod 755 /bin/xss_dev
+# Declare completion
 echo '
 
 Installation Complete.
